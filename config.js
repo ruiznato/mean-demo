e2fdbc0dc6a6d3a4d57b9256d770d2f95cc11b7b
Natalio Ruiz <natalio.ruiz@siriusxm.com>
2016-01-01 08:00:36 -0500

    Typo in db config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'port': process.env.PORT || 8080,
-    'database': 'mongodb://dbamin:dbadmin@ds037165.mongolab.com:37165/mean-demo-db',
+    'database': 'mongodb://dbadmin:dbadmin@ds037165.mongolab.com:37165/mean-demo-db',
     'secret': 'meandemoappmeandemoapp'
 };
